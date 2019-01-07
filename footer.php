a90ccc3578696bffe47f77e9ddbe73f4cceaa0e7
cferdinandi <chris@gomakethings.com>
2019-01-06 21:05:22 -0500

    Added check donation

--- footer.php
+++ footer.php
@@ -8,6 +8,12 @@
 ?>
 
 				<?php if (strtolower(get_bloginfo('description')) === 'donate') : ?>
+					<p class="margin-bottom-small">Or donate by check:</p>
+					<p>
+						PAWS New England<br>
+						PO Box 542<br>
+						Ashland, MA 01721
+					</p>
 					<p><em>PAWS New England is a 501(c)(3) non-profit group. All donations are tax deductible.</em></p>
 				<?php endif; ?>
 
